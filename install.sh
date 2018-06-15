0745732ce8cca078689902287860be1de55bb46e
joshrosso <joshrosso@gmail.com>
2018-06-15 06:19:27 -0600

    seperate root and non

--- install.sh
+++ install.sh
@@ -1,52 +1,39 @@
-pacman -Sy --needed \
-	networkmanager \
-	i3-wm \
-	rxvt-unicode \
-	dhclient \
-	network-manager-applet \
-	openconnect \
-	go \
-	bash-completion \
-	pass \
-	xorg \
-	xorg-xinit \
-	openssh \
-	ttf-inconsolata \
-	chromium \
-	firefox \
-	dmenu \
-	vim --asdeps lua \
-	alsa-utils \
-	volumeicon \
-	bluez \
-	bluez-utils \
-	arandr \
-	ctags \
-	ttf-symbola \
-	imagemagick \
-	mutt \
-	xf86-video-intel
+# must be non-root
+if [ ! -e ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+TEMP_DIR=$(mktemp -d)
+cd $TEMP_DIR
+
+if ! pacman -Qs spotify > /dev/null; then 
+	git clone https://aur.archlinux.org/spotify.git
+	cd spotify && makepkg -si && cd $TEMP_DIR
+fi
+
+if ! pacman -Qs slack > /dev/null; then 
+	git clone https://aur.archlinux.org/slack-desktop.git
+	cd slack-desktop && makepkg -si && cd $TEMP_DIR
+fi
+
+if ! pacman -Qs golang-dep > /dev/null; then 
+	git clone https://aur.archlinux.org/golang-dep.git
+	cd golang-dep && makepkg -si && cd $TEMP_DIR
+fi
+
+if ! pacman -Qs signal-desktop > /dev/null; then 
+	git clone https://aur.archlinux.org/signal-desktop-bin.git
+	cd signal-desktop-bin && makepkg -si && cd $TEMP_DIR
+fi
+
+if ! pacman -Qs kubectl > /dev/null; then 
+	git clone https://aur.archlinux.org/kubectl-bin.git
+	cd kubectl-bin && makepkg -si && cd $TEMP_DIR
+fi
 
 
-# todo (install vim-plug)
-# should not be root to do this
-#if [-f ~/.vim/autoload/plug.vim does not exist ]; then
-#  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#fi
-
-# TEMP_DIR=$(mktemp -d)
-
-# cd $TEMP_DIR
-#git clone https://aur.archlinux.org/spotify.git
-#git clone https://aur.archlinux.org/slack-desktop.git
-#git clone https://aur.archlinux.org/golang-dep.git
-#git clone https://aur.archlinux.org/signal-desktop-bin.git
-# git clone https://aur.archlinux.org/kubectl-bin.git
-#cd spotify && makepkg -si && cd $TEMP_DIR
-#cd slack-desktop && makepkg -si && cd $TEMP_DIR
-#cd golang-dep && makepkg -si && cd $TEMP_DIR
-#cd signal-desktop-bin && makepkg -si && cd $TEMP_DIR
-# cd kubectl-bin && makepkg -si && cd $TEMP_DIR
-
-# printf "[main]\ndhcp=dhclient" > /etc/NetworkManager/conf.d/dhcp-client.conf
+if ! pacman -Qs ttf-symbola > /dev/null; then 
+	git clone https://aur.archlinux.org/ttf-symbola.git
+	cd ttf-symbola && makepkg -si && cd $TEMP_DIR
+fi
---
+++ sudo-install.sh
@@ -0,0 +1,34 @@
+# requires sudo
+pacman -Sy --needed \
+	networkmanager \
+	i3-wm \
+	i3status \
+	i3blocks \
+	i3lock \
+	rxvt-unicode \
+	dhclient \
+	network-manager-applet \
+	openconnect \
+	go \
+	bash-completion \
+	pass \
+	xorg \
+	xorg-xinit \
+	openssh \
+	ttf-inconsolata \
+	chromium \
+	firefox \
+	dmenu \
+	vim --asdeps lua \
+	alsa-utils \
+	volumeicon \
+	bluez \
+	bluez-utils \
+	arandr \
+	ctags \
+	imagemagick \
+	mutt \
+	xf86-video-intel
+
+printf "[main]\ndhcp=dhclient" > /etc/NetworkManager/conf.d/dhcp-client.conf
+systemctl enable NetworkManager
